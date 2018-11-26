bbea85581f549c2489e85a0d1b5ab71430935935
Artur H. Lange <ArturLange@poczta.fm>
2018-11-26 16:43:23 +0100

    Add models

--- currencies/models.py
+++ currencies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Currency(models.Model):
+    # According to ISO 4217 this will always be three letters
+    code = models.CharField(max_length=3)
+
+class ExchangeRate(models.Model):
+    rate = models.DecimalField(max_digits=19, decimal_places=10) # Reasonable assumptions
+    date = models.DateField()
+
+    currency = models.ForeignKey(
+        Currency,
+        on_delete=models.CASCADE
+    )
